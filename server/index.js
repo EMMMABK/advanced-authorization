7945e7c151c571e9a0ea804b0afda6870a766647
EMMMABK <adilbikiev2@gmail.com>
2024-01-15 12:31:48 +0600

    add 2-commit

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express()
 
 const start = async() => {
