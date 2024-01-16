3ed542869ff6602cd13e3d7587376ecb8c9ed946
EMMMABK <adilbikiev2@gmail.com>
2024-01-16 17:27:05 +0600

    add 6-commit for Backend Func

--- server/service/token-service.js
+++ server/service/token-service.js
@@ -1,5 +1,25 @@
-class TokenService {
+const jwt = require('jsonwebtoken')
+const tokenModel = require('../models/token-model');
 
+class TokenService {
+    generateTokens(payload){
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '30m'})
+        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '30d'})
+        return{
+            accessToken,
+            refreshToken
+        }
+    }
+
+    async saveToken(userId, refreshToken){
+        const tokenData = await tokenModel.findOne({user: userId})
+        if(tokenData){
+            tokenData.refreshToken = refreshToken;
+            return tokenData.save();
+        }
+        const token = await tokenModel.create({user: userId, refreshToken})
+        return token;
+    }
 }
 
 module.exports = new TokenService();
--- server/service/user-service.js
+++ server/service/user-service.js
@@ -2,6 +2,8 @@ const UserModel = require('../models/user-model');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid')
 const mailService = require('./mail-service')
+const tokenService = require('./token-service')
+const UserDto = require('../dtos/user-dto')
 
 class UserService {
     async registration(email, password) {
@@ -13,7 +15,10 @@ class UserService {
         const activationLink = uuid.v4(); 
         const user = await UserModel.create({email, password: hashPassword, activationLink})
         await mailService.sendActivationMail(email, activationLink);
-         
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({...userDto});
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+        return {...tokens, user: userDto}
     }
 }
 
