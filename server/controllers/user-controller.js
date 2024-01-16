f8d9dc0a474a27051fdac1db4d656e6a09bcee25
EMMMABK <adilbikiev2@gmail.com>
2024-01-16 18:28:55 +0600

    add 7-commit backend part

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,9 +1,14 @@
+const userService = require('../service/user-service');
+
 class UserController {
     async registration(req, res, next){
         try{
-
+            const {email, password} = req.body;
+            const userData = await userService.registration(email, password);
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            return res.json(userData);
         } catch(e) {
-
+            console.log(e);
         }
     }
     async login(req, res, next){
