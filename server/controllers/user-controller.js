b659ea0d4531279b2df3dc5b9a11e5d9518a7258
EMMMABK <adilbikiev2@gmail.com>
2024-01-16 16:18:30 +0600

    add 4-commit

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,46 @@
+class UserController {
+    async registration(req, res, next){
+        try{
+
+        } catch(e) {
+
+        }
+    }
+    async login(req, res, next){
+        try{
+
+        } catch(e) {
+            
+        }
+    }
+    async logout(req, res, next){
+        try{
+
+        } catch(e) {
+            
+        }
+    }
+    async activate(req, res, next){
+        try{
+
+        } catch(e) {
+            
+        }
+    }
+    async refresh(req, res, next){
+        try{
+
+        } catch(e) {
+            
+        }
+    }
+    async getUsers(req, res, next){
+        try{
+            res.json(['123', '456'])
+        } catch(e) {
+            
+        }
+    }
+}
+
+module.exports = new UserController();
--- server/index.js
+++ server/index.js
@@ -2,12 +2,23 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose');
+const router = require('./router/index')
 
 const PORT = process.env.PORT || 5000;
 const app = express()
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use('/api', router);
+
 const start = async() => {
     try{
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
         app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`))
     } catch (e){
         console.log(e);
