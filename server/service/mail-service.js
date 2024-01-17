66a1e78b2aba96cb5317bed717174e56ec7d8db5
EMMMABK <adilbikiev2@gmail.com>
2024-01-17 11:33:54 +0600

    add Confirmation in mail

--- server/service/mail-service.js
+++ server/service/mail-service.js
@@ -5,12 +5,30 @@ class MailService {
 
     constructor(){
         this.transporter = nodemailer.createTransport({
-            
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: false,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD
+            }
         })
     }
 
     async sendActivationMail(to, link){
-
+        await this.transporter.sendMail({
+            from: process.env.SMTP_USER,
+            to,
+            subject: 'Activation account on ' + process.env.API_URL,
+            text: '',
+            html: 
+                `
+                    <div>
+                        <h1>For activation your account click this link</h1>
+                        <a href="${link}">${link}</a> 
+                    </div>   
+                `
+        })
     }
 }
 
--- server/service/user-service.js
+++ server/service/user-service.js
@@ -14,7 +14,7 @@ class UserService {
         const hashPassword = await bcrypt.hash(password, 3)
         const activationLink = uuid.v4(); 
         const user = await UserModel.create({email, password: hashPassword, activationLink})
-        await mailService.sendActivationMail(email, activationLink);
+        await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
         const userDto = new UserDto(user);
         const tokens = tokenService.generateTokens({...userDto});
         await tokenService.saveToken(userDto.id, tokens.refreshToken);
