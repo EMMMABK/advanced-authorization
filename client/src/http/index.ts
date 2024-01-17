8bc20f5eb505936b4cc1bee2b2f854cd4e91841b
EMMMABK <adilbikiev2@gmail.com>
2024-01-17 13:09:37 +0600

    add 19-commit Add Token to Request

---
+++ client/src/http/index.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const API_URL = 'http://localhost:5000/api'
+const $api = axios.create({
+    withCredentials: true,
+    baseURL: API_URL
+})
+
+$api.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+    return config;
+})
+
+export default $api;
