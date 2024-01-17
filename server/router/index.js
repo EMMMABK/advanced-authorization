acf51d6dd28846ddd9396c845c4168b96c659361
EMMMABK <adilbikiev2@gmail.com>
2024-01-17 13:00:12 +0600

    add 17-commit Backend Part Finish

--- server/router/index.js
+++ server/router/index.js
@@ -2,6 +2,7 @@ const Router = require('express').Router;
 const userController = require('../controllers/user-controller')
 const router = new Router();
 const {body} = require('express-validator');
+const authMiddleware = require('../middlewares/auth-middleware')
 
 router.post('/registration',
     body('email').isEmail(),
@@ -12,6 +13,6 @@ router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
 router.get('/refresh', userController.refresh);
-router.get('/users', userController.getUsers);
+router.get('/users', authMiddleware, userController.getUsers);
 
 module.exports = router
--- server/service/token-service.js
+++ server/service/token-service.js
@@ -3,7 +3,7 @@ const tokenModel = require('../models/token-model');
 
 class TokenService {
     generateTokens(payload){
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '30m'})
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '30s'})
         const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '30d'})
         return{
             accessToken,
